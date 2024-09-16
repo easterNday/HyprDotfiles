3500999509b1e73214bfc079662c5d5850252083
easterNday <849919718@qq.com>
2024-09-16 18:16:26 +0800

    🌷 UI(ags): replace mako with builtin notification

--- ags/config.js
+++ ags/config.js
@@ -1,10 +1,12 @@
 import { Bar } from "./modules/bar.js"
 import { Launcher } from "./modules/launcher.js"
+import { Notifications } from "./modules/popups/Notification.js"
 
 App.config({
     style: "./style.css",
     windows: [
         Bar(),
-        Launcher
+        Launcher,
+        Notifications()
     ]
 })
